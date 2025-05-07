8938bbbfc5f05a77257d966740ec03e263dee5d2
Chris Nanda <chris.nanda@sonisoft.io>
2025-05-07 14:44:36 -0700

    JIRA-Num : Updated script include to have updateToDo method

--- src/includes/sys_script_include/ToDoHelper.server.js
+++ src/includes/sys_script_include/ToDoHelper.server.js
@@ -10,5 +10,14 @@ ToDoHelper.prototype = {
 		return grToDo;
 	},
 
+    updateToDo: function(sysId, field, value){
+        var grToDo = new GlideRecord("x_502054_sdk_cnvd_todo_task");
+        if (grToDo.get(sysId)) {
+            grToDo.setValue(field, value);
+            grToDo.update();
+        }
+        return grToDo;
+    },
+
     type: 'ToDoHelper'
 };
